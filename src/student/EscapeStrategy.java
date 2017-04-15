78889d3e7a175fccd5dce2463c706d51b09fe198
Matthew Keri <mmkeri@gmail.com>
2017-04-15 19:51:21 +0100

    Escape strategy gets you out but not perfected to give maximum gold

--- src/student/EscapeStrategy.java
+++ src/student/EscapeStrategy.java
@@ -1,5 +1,6 @@
 package student;
 
+import game.Edge;
 import game.EscapeState;
 import game.Node;
 import game.NodeStatus;
@@ -15,13 +16,9 @@ public class EscapeStrategy {
     private Collection<Node> mazeVertices = new TreeSet<>();
     private Node exitNode;
     private List<Node> currentPath = new LinkedList<>();
-    private List<Node> repeatedPath = new LinkedList<>();
-    private Set<Node> visitedNodes = new TreeSet<>(new NodeComparator());
-    private List<Node> bestPath = null;
-    private List<List<Node>> collectionOfPaths = new LinkedList<>();
-    private int maxDistance = 0;
-    private int maxGold = 0;
-    private int initialTimeRemaining = 0;
+    private Set<Node> visitedNodes = new HashSet<>();
+    private HashMap<Node, Integer> weightedNodes = new HashMap<>();
+    private Stack<Node> exitPath = new Stack<>();
 
     public EscapeStrategy(EscapeState state) {
         this.state = state;
@@ -30,66 +27,98 @@ public class EscapeStrategy {
         mazeVertices = state.getVertices();
         exitNode = state.getExit();
         Node startNode = state.getCurrentNode();
-        initialTimeRemaining = state.getTimeRemaining();
-        walkTheGraph(startNode, currentPath, visitedNodes);
-        System.out.println("just finished walking the graph");
-        bestPath.remove(startNode);
-        bestPath.add(exitNode);
-        for(Node n: bestPath){
+        initializeUnvisitedNodeMap(weightedNodes, mazeVertices);
+        setStartNode(startNode);
+        dijktrasPath(startNode);
+        int exitValue = weightedNodes.get(exitNode);
+        reverseWalkPath(exitNode, exitValue);
+        exitPath.pop();
+        while(!exitPath.empty()) {
+            Node n = exitPath.pop();
             state.moveTo(n);
-            if(n.getTile().getGold() > 0){
+            if (n.getTile().getGold() > 0) {
                 state.pickUpGold();
             }
         }
-        state.getExit();
+    }
+
+
+    private void initializeUnvisitedNodeMap(HashMap<Node, Integer> weightedNodes, Collection<Node> vertices) {
+        for (Node n : vertices) {
+            weightedNodes.put(n, Integer.MAX_VALUE);
+        }
+    }
+
+    private void setStartNode(Node startNode){
+        weightedNodes.replace(startNode, Integer.MAX_VALUE, 0);
+    }
+
+    private void reverseWalkPath(Node exitNode, int exitWeight){
+        Collection<Edge> currentEdges = exitNode.getExits();
+        Node nextNode = null;
+        int newExitWeight = 0;
+        if(exitWeight == 0){
+            exitPath.push(exitNode);
+            return;
+        }
+        for(Edge e : currentEdges){
+            Node n = e.getDest();
+            if(exitWeight - (exitNode.getEdge(n).length()) == weightedNodes.get(n)){
+                nextNode = n;
+                newExitWeight = exitWeight - (exitNode.getEdge(n).length());
+            }
+        }
+        exitPath.push(exitNode);
+        reverseWalkPath(nextNode, newExitWeight);
+    }
+
+    private void dijktrasPath(Node current){
+        int minDistance = Integer.MAX_VALUE;
+        Node nextNodeToTry = null;
+        if(current.equals(exitNode)){
+            return;
+        }
+        Collection<Edge> neighbours = new HashSet<>(current.getExits());
+
+        int currentNodeWeight = weightedNodes.get(current);
+        for(Iterator<Edge> itr = neighbours.iterator();
+                itr.hasNext();){
+            Edge e = itr.next();
+            if(visitedNodes.contains(e.getDest())){
+                itr.remove();
+            }
+        }
+        for(Edge e : neighbours){
+            int edgeWeight = e.length();
+            int newWeight = currentNodeWeight + edgeWeight;
+            int destinationNodeCurrentWeight = weightedNodes.get(e.getDest());
+
+            Node thisNode = e.getDest();
+            if(newWeight < destinationNodeCurrentWeight){
+                weightedNodes.replace(thisNode, (newWeight));
+            }
+        }
+        visitedNodes.add(current);
+        for(Map.Entry<Node, Integer> e: weightedNodes.entrySet()){
+            if(e.getValue() < minDistance && !visitedNodes.contains(e.getKey())){
+                nextNodeToTry = e.getKey();
+                minDistance = e.getValue();
+            }
+        }
+        dijktrasPath(nextNodeToTry);
     }
 
     class NodeComparator implements Comparator<Node> {
         @Override
         public int compare(Node o1, Node o2) {
-            if(o1.getId() == o2.getId()) {
+            long diff = o1.getId() - o2.getId();
+            if (diff == 0) {
                 return 0;
-            } else {
+            } else if (diff < 0) {
                 return -1;
+            } else {
+                return +1;
             }
         }
     };
-
-    private void walkTheGraph(Node n, List<Node> currentPath, Set<Node> visitedNodes){
-        visitedNodes.add(n);
-        if(currentPath.size() >= mazeVertices.size()){
-            return;
-        }
-        if (repeatedPath.contains(n)) {
-            currentPath.remove(n);
-            return;
-        }
-        if (currentPath.contains(n)) {
-            repeatedPath.add(n);
-        }
-        currentPath.add(n);
-        if(n == exitNode){
-            List<Node> copyOfPath = new LinkedList<>(currentPath);
-            scoreCurrentPath(copyOfPath);
-            copyOfPath.remove(n);
-            return;
-        }
-        for(Node child: n.getNeighbours()){
-            walkTheGraph(child, currentPath, visitedNodes);
-        }
-        currentPath.remove(n);
-    }
-
-    private void scoreCurrentPath(List<Node> path){
-        collectionOfPaths.add(path);
-        int distance = 0;
-        int gold = 0;
-        for(int i = 0; i < path.size() -1; i ++){
-            gold = gold + path.get(i).getTile().getGold();
-            distance = distance + path.get(i).getEdge(path.get(i + 1)).length();
-        }
-        if(distance >= maxDistance && distance <= initialTimeRemaining && gold >= maxGold) {
-            bestPath = path;
-        }
-    }
 }
