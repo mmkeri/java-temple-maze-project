1df88a1f06ba74d1945835045a8b9bcdf8e190f0
Matthew Keri <mmkeri@gmail.com>
2017-04-15 20:41:28 +0100

    Clean up of classes and Escape method

---
+++ src/student/DijkstraAlgo.java
@@ -0,0 +1,68 @@
+package student;
+
+import game.Edge;
+import game.Node;
+
+import java.util.*;
+
+/**
+ * Created by mmkeri on 15/04/2017.
+ */
+public class DijkstraAlgo {
+
+    private Node exitNode;
+    private HashMap<Node, Integer> weightedNodes = new HashMap<>();
+    private Set<Node> visitedNodes = new HashSet<>();
+
+    public void setExitNode(Node exitNode){
+        this.exitNode = exitNode;
+    }
+
+    public void setWeightedNodes(HashMap<Node, Integer> weightedNodes){
+        this.weightedNodes = weightedNodes;
+    }
+
+    public HashMap<Node, Integer> testPaths(Node currentNode, Node targetNode, HashMap<Node, Integer> initialWeightedNodes){
+        setExitNode(targetNode);
+        setWeightedNodes(initialWeightedNodes);
+        dijktrasPath(currentNode);
+        return weightedNodes;
+    }
+
+    private void dijktrasPath(Node current){
+        int minDistance = Integer.MAX_VALUE;
+        Node nextNodeToTry = null;
+        if(current.equals(exitNode)){
+            return;
+        }
+        Collection<Edge> neighbours = new HashSet<>(current.getExits());
+
+        int currentNodeWeight = weightedNodes.get(current);
+        for(Iterator<Edge> itr = neighbours.iterator();
+            itr.hasNext();){
+            Edge e = itr.next();
+            if(visitedNodes.contains(e.getDest())){
+                itr.remove();
+            }
+        }
+        for(Edge e : neighbours){
+            int edgeWeight = e.length();
+            int newWeight = currentNodeWeight + edgeWeight;
+            int destinationNodeCurrentWeight = weightedNodes.get(e.getDest());
+
+            Node thisNode = e.getDest();
+            if(newWeight < destinationNodeCurrentWeight){
+                weightedNodes.replace(thisNode, (newWeight));
+            }
+        }
+        visitedNodes.add(current);
+        for(Map.Entry<Node, Integer> e: weightedNodes.entrySet()){
+            if(e.getValue() < minDistance && !visitedNodes.contains(e.getKey())){
+                nextNodeToTry = e.getKey();
+                minDistance = e.getValue();
+            }
+        }
+        dijktrasPath(nextNodeToTry);
+    }
+
+}
--- src/student/EscapeStrategy.java
+++ src/student/EscapeStrategy.java
@@ -15,33 +15,34 @@ public class EscapeStrategy {
     private EscapeState state;
     private Collection<Node> mazeVertices = new TreeSet<>();
     private Node exitNode;
-    private List<Node> currentPath = new LinkedList<>();
-    private Set<Node> visitedNodes = new HashSet<>();
     private HashMap<Node, Integer> weightedNodes = new HashMap<>();
-    private Stack<Node> exitPath = new Stack<>();
+    private HashMap<Node, Integer> testedWeightedNodes;
+    private Stack<Node> reversePath;
 
     public EscapeStrategy(EscapeState state) {
         this.state = state;
     }
     public void doEscape() {
+        DijkstraAlgo pathTester = new DijkstraAlgo();
+        ReversePath backwardsPath = new ReversePath();
         mazeVertices = state.getVertices();
         exitNode = state.getExit();
         Node startNode = state.getCurrentNode();
         initializeUnvisitedNodeMap(weightedNodes, mazeVertices);
         setStartNode(startNode);
-        dijktrasPath(startNode);
-        int exitValue = weightedNodes.get(exitNode);
-        reverseWalkPath(exitNode, exitValue);
-        exitPath.pop();
-        while(!exitPath.empty()) {
-            Node n = exitPath.pop();
+        testedWeightedNodes = pathTester.testPaths(startNode, exitNode, weightedNodes);
+        int exitValue = testedWeightedNodes.get(exitNode);
+        reversePath = backwardsPath.plotReversePath(exitNode, exitValue, testedWeightedNodes);
+
+        reversePath.pop();
+        while(!reversePath.empty()) {
+            Node n = reversePath.pop();
             state.moveTo(n);
             if (n.getTile().getGold() > 0) {
                 state.pickUpGold();
             }
         }
     }
-
 
     private void initializeUnvisitedNodeMap(HashMap<Node, Integer> weightedNodes, Collection<Node> vertices) {
         for (Node n : vertices) {
@@ -53,72 +54,4 @@ public class EscapeStrategy {
         weightedNodes.replace(startNode, Integer.MAX_VALUE, 0);
     }
 
-    private void reverseWalkPath(Node exitNode, int exitWeight){
-        Collection<Edge> currentEdges = exitNode.getExits();
-        Node nextNode = null;
-        int newExitWeight = 0;
-        if(exitWeight == 0){
-            exitPath.push(exitNode);
-            return;
-        }
-        for(Edge e : currentEdges){
-            Node n = e.getDest();
-            if(exitWeight - (exitNode.getEdge(n).length()) == weightedNodes.get(n)){
-                nextNode = n;
-                newExitWeight = exitWeight - (exitNode.getEdge(n).length());
-            }
-        }
-        exitPath.push(exitNode);
-        reverseWalkPath(nextNode, newExitWeight);
-    }
-
-    private void dijktrasPath(Node current){
-        int minDistance = Integer.MAX_VALUE;
-        Node nextNodeToTry = null;
-        if(current.equals(exitNode)){
-            return;
-        }
-        Collection<Edge> neighbours = new HashSet<>(current.getExits());
-
-        int currentNodeWeight = weightedNodes.get(current);
-        for(Iterator<Edge> itr = neighbours.iterator();
-                itr.hasNext();){
-            Edge e = itr.next();
-            if(visitedNodes.contains(e.getDest())){
-                itr.remove();
-            }
-        }
-        for(Edge e : neighbours){
-            int edgeWeight = e.length();
-            int newWeight = currentNodeWeight + edgeWeight;
-            int destinationNodeCurrentWeight = weightedNodes.get(e.getDest());
-
-            Node thisNode = e.getDest();
-            if(newWeight < destinationNodeCurrentWeight){
-                weightedNodes.replace(thisNode, (newWeight));
-            }
-        }
-        visitedNodes.add(current);
-        for(Map.Entry<Node, Integer> e: weightedNodes.entrySet()){
-            if(e.getValue() < minDistance && !visitedNodes.contains(e.getKey())){
-                nextNodeToTry = e.getKey();
-                minDistance = e.getValue();
-            }
-        }
-        dijktrasPath(nextNodeToTry);
-    }
-
-    class NodeComparator implements Comparator<Node> {
-        @Override
-        public int compare(Node o1, Node o2) {
-            long diff = o1.getId() - o2.getId();
-            if (diff == 0) {
-                return 0;
-            } else if (diff < 0) {
-                return -1;
-            } else {
-                return +1;
-            }
-        }
-    };
 }
